86a79881f5641822394dc0f0d62528980ec4b305
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2021-01-14 15:01:48 +0000

    Created component for single category with list of products

---
+++ src/containers/Category/Category.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Category = () => {
+  const {
+    categoriesState: { categories },
+  } = useSelector(state => state);
+  const componentIsMounted = useRef(true);
+  const [objCategory, setObjCategory] = useState(null);
+  const { id } = useParams();
+  const objFound = categories.find(item => item.id === parseInt(id, 10));
+  useEffect(() => {
+    const fetchCategoryData = () => {
+      if (componentIsMounted.current) {
+        setObjCategory(objFound);
+      } else {
+        setObjCategory(objFound);
+      }
+    };
+    fetchCategoryData();
+    return () => {
+      componentIsMounted.current = false;
+    };
+  }, [categories, id, objFound]);
+  if (objCategory) {
+    return <div>Hello I am category {objCategory.title}</div>;
+  }
+  return null;
+};
+
+export default Category;
