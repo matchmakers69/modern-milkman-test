2692775ea95d4a0e691bbf43da4f2831b2eab5ee
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2021-01-14 12:24:00 +0000

    Modified package.json -added proxy, updated api.js file

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,1 @@
-const { REACT_APP_CATEGORIES_URL } = process.env;
-console.log(REACT_APP_CATEGORIES_URL);
-
-export const CATEGORIES_URL = REACT_APP_CATEGORIES_URL;
+export const CATEGORIES_URL = '/categoryProducts';
