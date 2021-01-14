36ddb25615a7f2e933aa38c7b9b0654814c7a5d9
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2021-01-14 12:28:39 +0000

    Updated Home.js - removed unused useEffect

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,24 +1,7 @@
 import React, { useEffect, useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchCategoriesData } from 'store/categories/actions';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const unmounted = useRef(false);
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      if (!unmounted.current) {
-        await dispatch(fetchCategoriesData());
-      }
-    };
-    fetchCategories();
-
-    return () => {
-      unmounted.current = true;
-    };
-  }, [dispatch]);
-
   return <div>home</div>;
 };
 
