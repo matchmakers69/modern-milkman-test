4a1383afbf6cf20e0747cb1e015c957beebeee5a
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2021-01-15 11:51:59 +0000

    Updated App.js - moved navigation component to Layout component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import React from 'react';
 import Routes from 'routes';
 import { Switch } from 'react-router-dom';
 import MainTemplate from 'templates/MainTemplate';
-import NavigationSidebar from 'components/common/NavigationSidebar';
 
 const App = () => {
   return (
     <MainTemplate>
-      <>
-        <NavigationSidebar />
-
-        <Switch>
-          <Routes />
-        </Switch>
-      </>
+      <Switch>
+        <Routes />
+      </Switch>
     </MainTemplate>
   );
 };
